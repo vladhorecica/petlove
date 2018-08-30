43233c86e4f4eebdf9be351532ec871c51fe14ee
VHorecica <vhorecica@pentalog.com>
2018-08-30 17:16:55 +0300

    Add node container and Backend UI demo app.

---
+++ application/ui/backend/src/app/components/tabPanel/index.js
@@ -0,0 +1,7 @@
+// @flow weak
+
+export { default as TabPanel }            from './TabPanel/TabPanel';
+export { default as TabPanelHeader }      from './tabPanelHeader/TabPanelHeader';
+export { default as TabPanelBody }        from './tabPanelBody/TabPanelBody';
+export { default as TabPanelBodyContent } from './TabPanelBodyContent/tabPanelBodyContent';
+export { default as TabPanelDemo }        from './TabPanelDemo';
